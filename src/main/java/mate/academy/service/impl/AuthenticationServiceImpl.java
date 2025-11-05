bbf00d9ca81f51cbebabb3b37ce18ff6c9b0ee4b
Serhiy <sergiy.kusiv@gmail.com>
2025-11-05 14:27:58 +0200

    Implemented AuthenticationServiceImpl

--- src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/impl/AuthenticationServiceImpl.java
@@ -2,17 +2,49 @@ package mate.academy.service.impl;
 
 import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
+import mate.academy.service.UserService;
+import mate.academy.util.PasswordUtil;
 
+import java.util.Optional;
+
+@Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    @Inject
+    private UserService userService;
+
     @Override
     public User login(String email, String password) throws AuthenticationException {
-        return null;
+        Optional<User> userOptional = userService.findByEmail(email);
+
+        if (userOptional.isEmpty() || !PasswordUtil.isPasswordValid(
+                password, userOptional.get().getPassword(),
+                userOptional.get().getSalt())) {
+           throw new AuthenticationException("Invalid email or password");
+        }
+
+        return userOptional.get();
     }
 
     @Override
     public User register(String email, String password) throws RegistrationException {
-        return null;
+        if (userService.findByEmail(email).isPresent()) {
+            throw new RegistrationException("User with email " + email + " already exists.");
+        }
+        User user = new User();
+        user.setEmail(email);
+
+        byte[] salt = PasswordUtil.getSalt();
+        user.setSalt(salt);
+        user.setPassword(PasswordUtil.hashPassword(password, salt));
+
+        try {
+            return userService.add(user);
+        } catch (Exception e) {
+            throw new RegistrationException("Could not register user with email: " + email, e);
+        }
     }
 }
--- src/main/java/mate/academy/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -1,18 +1,21 @@
 package mate.academy.service.impl;
 
+import mate.academy.dao.UserDao;
 import mate.academy.model.User;
 import mate.academy.service.UserService;
 
 import java.util.Optional;
 
 public class UserServiceImpl implements UserService {
+    private UserDao userDao;
+
     @Override
     public User add(User user) {
-        return null;
+        return userDao.add(user);
     }
 
     @Override
     public Optional<User> findByEmail(String email) {
-        return Optional.empty();
+        return userDao.findByEmail(email);
     }
 }
