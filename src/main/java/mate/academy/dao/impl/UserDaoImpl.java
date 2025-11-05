ae98410293852ee0aa2eff84dac85d7eaef67106
Serhiy <sergiy.kusiv@gmail.com>
2025-11-05 14:04:25 +0200

    created necessary files + implemented my custom exceptions

---
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -0,0 +1,7 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+
+}
---
+++ src/main/java/mate/academy/model/User.java
@@ -0,0 +1,27 @@
+package mate.academy.model;
+
+public class User {
+    String password;
+    String email;
+
+    public User(String password, String email) {
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
